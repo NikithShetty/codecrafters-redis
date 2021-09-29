53a1412639fa3dbdfe711ef003572b688e98f8c7
Nikith Shetty <shettynikith3@gmail.com>
2021-09-29 23:58:38 +0530

    pass the first stage

--- app/server.go
+++ app/server.go
@@ -1,25 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Your code goes here!")
+	fmt.Println("Starting tcp server..")
 
-	// Uncomment this block to pass the first stage
-	//
-	// l, err := net.Listen("tcp", "0.0.0.0:6379")
-	// if err != nil {
-	// 	fmt.Println("Failed to bind to port 6379")
-	// 	os.Exit(1)
-	// }
-	// _, err = l.Accept()
-	// if err != nil {
-	// 	fmt.Println("Error accepting connection: ", err.Error())
-	// 	os.Exit(1)
-	// }
+	port := flag.String("Port number", "6379", "Port number to which redis server should bind to.")
+	addr := "0.0.0.0:" + *port
+
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+	_, err = l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
 }
